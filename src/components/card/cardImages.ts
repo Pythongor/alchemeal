6290a3ef8ff424e68f5047e57fd97b5fd15d1da0
pythongor <djmozartd@gmail.com>
2022-02-20 21:58:09 +0100

    add new elements and change food types

--- src/components/card/cardImages.ts
+++ src/components/card/cardImages.ts
@@ -9,8 +9,9 @@ import egg from "assets/images/icons/egg.png";
 import meat from "assets/images/icons/meat.png";
 import milk from "assets/images/icons/milk.png";
 import kefir from "assets/images/icons/kefir.png";
-import sourCream from "assets/images/icons/sour cream.png";
+import cream from "assets/images/icons/cream.png";
 import cheese from "assets/images/icons/cheese.png";
+import blueCheese from "assets/images/icons/blue cheese.png";
 import water from "assets/images/icons/water.png";
 import boilingWater from "assets/images/icons/boiling water.png";
 import seed from "assets/images/icons/seed.png";
@@ -21,9 +22,63 @@ import omelette from "assets/images/icons/omelette.png";
 import fish from "assets/images/icons/fish.png";
 import arable from "assets/images/icons/arable.png";
 import boiledEgg from "assets/images/icons/boiled egg.png";
-import boiledMeat from "assets/images/icons/boiled meat.png";
+import roast from "assets/images/icons/roast.png";
 import condensedMilk from "assets/images/icons/condensed milk.png";
 import burnedSomething from "assets/images/icons/burned something.png";
+import sausage from "assets/images/icons/sausage.png";
+import grass from "assets/images/icons/grass.png";
+import fishSoup from "assets/images/icons/fish soup.png";
+import tea from "assets/images/icons/tea.png";
+import cutlet from "assets/images/icons/cutlet.png";
+import koumiss from "assets/images/icons/koumiss.png";
+import fondue from "assets/images/icons/fondue.png";
+import friedFish from "assets/images/icons/fried fish.png";
+import cereal from "assets/images/icons/cereal.png";
+import manure from "assets/images/icons/manure.png";
+import spices from "assets/images/icons/spices.png";
+import surströmming from "assets/images/icons/surströmming.png";
+import bush from "assets/images/icons/bush.png";
+import flower from "assets/images/icons/flower.png";
+import mojito from "assets/images/icons/mojito.png";
+import iceTea from "assets/images/icons/ice tea.png";
+import flour from "assets/images/icons/flour.png";
+import kvass from "assets/images/icons/kvass.png";
+import porridge from "assets/images/icons/porridge.png";
+import berry from "assets/images/icons/berry.png";
+import sauce from "assets/images/icons/sauce.png";
+import tree from "assets/images/icons/tree.png";
+import nut from "assets/images/icons/nut.png";
+import honey from "assets/images/icons/honey.png";
+import dough from "assets/images/icons/dough.png";
+import beer from "assets/images/icons/beer.png";
+import pilaf from "assets/images/icons/pilaf.png";
+import sushi from "assets/images/icons/sushi.png";
+import juice from "assets/images/icons/juice.png";
+import jam from "assets/images/icons/jam.png";
+import wine from "assets/images/icons/wine.png";
+import yogurt from "assets/images/icons/yogurt.png";
+import mayonnaise from "assets/images/icons/mayonnaise.png";
+import coffeeBean from "assets/images/icons/coffee bean.png";
+import cocoaBean from "assets/images/icons/cocoa bean.png";
+import fruit from "assets/images/icons/fruit.png";
+import reed from "assets/images/icons/reed.png";
+import sugar from "assets/images/icons/sugar.png";
+import caramel from "assets/images/icons/caramel.png";
+import syrup from "assets/images/icons/syrup.png";
+import eggnog from "assets/images/icons/eggnog.png";
+import butterDough from "assets/images/icons/butter dough.png";
+import milkshake from "assets/images/icons/milkshake.png";
+import iceCream from "assets/images/icons/ice cream.png";
+import candy from "assets/images/icons/candy.png";
+import cottonCandy from "assets/images/icons/cotton candy.png";
+import cremeBrulee from "assets/images/icons/creme brulee.png";
+import bun from "assets/images/icons/bun.png";
+import peanutButter from "assets/images/icons/peanut butter.png";
+import muesli from "assets/images/icons/muesli.png";
+import gozinaki from "assets/images/icons/gozinaki.png";
+import mead from "assets/images/icons/mead.png";
+import cookie from "assets/images/icons/cookie.png";
+import gingerbread from "assets/images/icons/gingerbread.png";
 
 const images: { [key in ElementType]?: string } = {
   earth,
@@ -36,8 +91,9 @@ const images: { [key in ElementType]?: string } = {
   meat,
   milk,
   kefir,
-  "sour cream": sourCream,
+  cream,
   cheese,
+  "blue cheese": blueCheese,
   water,
   "boiling water": boilingWater,
   seed,
@@ -48,9 +104,63 @@ const images: { [key in ElementType]?: string } = {
   fish,
   arable,
   "boiled egg": boiledEgg,
-  "boiled meat": boiledMeat,
+  roast,
   "condensed milk": condensedMilk,
   "burned something": burnedSomething,
+  sausage,
+  grass,
+  "fish soup": fishSoup,
+  tea,
+  cutlet,
+  koumiss,
+  fondue,
+  "fried fish": friedFish,
+  cereal,
+  manure,
+  spices,
+  surströmming,
+  bush,
+  flower,
+  mojito,
+  "ice tea": iceTea,
+  flour,
+  kvass,
+  porridge,
+  berry,
+  sauce,
+  tree,
+  nut,
+  honey,
+  dough,
+  beer,
+  pilaf,
+  sushi,
+  juice,
+  jam,
+  wine,
+  yogurt,
+  mayonnaise,
+  "coffee bean": coffeeBean,
+  "cocoa bean": cocoaBean,
+  fruit,
+  reed,
+  sugar,
+  caramel,
+  syrup,
+  eggnog,
+  "butter dough": butterDough,
+  milkshake,
+  "ice cream": iceCream,
+  candy,
+  "cotton candy": cottonCandy,
+  "creme brulee": cremeBrulee,
+  bun,
+  "peanut butter": peanutButter,
+  muesli,
+  gozinaki,
+  mead,
+  cookie,
+  gingerbread,
 };
 
 export default images;
